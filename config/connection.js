a87e680663796ddc5efb5fc5dd6546ccf92a26c7
Mike Feller <41796396+MikeF-Code@users.noreply.github.com>
2019-01-15 19:49:40 -0500

    Added Heroku connection info

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 const mysql = require("mysql");
 
 // Map mysql connection info to 'connection' variable for later use.
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
